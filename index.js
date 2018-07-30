82ce7eab61341ec35f0e983fa0fccde628b30f56
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-30 18:57:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,3 +33,7 @@ function showRepositories(event, data) {
   const repoList = template(repos)
   document.getElementById("repositories").innerHTML = repoList
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  Handlebars.registerPartial("authorPartial", document.getElementById("author-partial-template").innerHTML)
+});
